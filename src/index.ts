f04c07933d72ec27d984ccd7bd6c4a6b626c6a2b
Simon Mumenthaler <simon.mumenthaler@shiftcode.io>
2025-09-09 12:05:07 +0200

    style(*): format

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,55 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {StackHelper} from './stack-helper.js'
-import {isMainBranch, isMasterBranch, parseBranchName} from '@shiftcode/branch-utilities'
+import { StackHelper } from './stack-helper.js'
+import { isMainBranch, isMasterBranch, parseBranchName } from '@shiftcode/branch-utilities'
 
 try {
-    // reading the inputs (inputs defined in action.yml)
-    const waitForDeleteComplete = core.getInput('waitForDeleteComplete', {required: true}) === 'true'
-    const stackNamePrefix = core.getInput('stackNamePrefix', {required: true})
-    const ignoreBranches: string[] = JSON.parse(core.getInput('ignoreBranches', {required: true}))
+  // reading the inputs (inputs defined in action.yml)
+  const waitForDeleteComplete = core.getInput('waitForDeleteComplete', { required: true }) === 'true'
+  const stackNamePrefix = core.getInput('stackNamePrefix', { required: true })
+  const ignoreBranches: string[] = JSON.parse(core.getInput('ignoreBranches', { required: true }))
 
-    if (!Array.isArray(ignoreBranches)) {
-        throw new Error(
-            `action input 'ignoreBranches' needs to be a json array. provided value '${core.getInput(
-                'ignoreBranches',
-            )}' could not be parsed`,
-        )
+  if (!Array.isArray(ignoreBranches)) {
+    throw new Error(
+      `action input 'ignoreBranches' needs to be a json array. provided value '${core.getInput(
+        'ignoreBranches',
+      )}' could not be parsed`,
+    )
+  }
+
+  // this is defined, although the payload.ref is not typed, it contains the branch name
+  const branchName = github.context.payload.ref
+
+  if (isMasterBranch(branchName) || isMainBranch(branchName)) {
+    core.notice(`detected master branch --> stopping here`)
+  } else if (ignoreBranches.includes(branchName)) {
+    core.notice(`branch '${github.context.payload.ref}' was defined to ignore --> stopping here`)
+  } else {
+    const branch = parseBranchName(branchName.replace(/^(.+\/)?/, ''))
+    const xxSuffix = `xx${branch.branchId}`
+    const prSuffix = `pr${branch.branchId}`
+
+    core.info(`stack name  ${stackNamePrefix}-(${xxSuffix}|${prSuffix})`)
+
+    const stackHelper = new StackHelper()
+
+    const stacks = (await stackHelper.listAllStacks(stackNamePrefix)).map((s) => s.StackName)
+
+    const xxStacks = stacks.filter((stackName) => stackName.endsWith(xxSuffix))
+    const prStacks = stacks.filter((stackName) => stackName.endsWith(prSuffix))
+
+    if (!xxStacks.length && !prStacks.length) {
+      core.notice('No Stacks to delete')
+    } else {
+      await Promise.allSettled([
+        stackHelper.deleteStacks(xxStacks, waitForDeleteComplete),
+        stackHelper.deleteStacks(prStacks, waitForDeleteComplete),
+      ])
+      core.notice(
+        `deleted stacks: ${[...xxStacks, ...prStacks].join(', ')} (waitForDeleteComplete: ${waitForDeleteComplete})`,
+      )
     }
-
-    // this is defined, although the payload.ref is not typed, it contains the branch name
-    const branchName = github.context.payload.ref
-
-    if (isMasterBranch(branchName) || isMainBranch(branchName)) {
-        core.notice(`detected master branch --> stopping here`)
-    }else if (ignoreBranches.includes(branchName)) {
-        core.notice(`branch '${github.context.payload.ref}' was defined to ignore --> stopping here`)
-    }else {
-        const branch = parseBranchName(branchName.replace(/^(.+\/)?/, ''))
-        const xxSuffix = `xx${branch.branchId}`
-        const prSuffix = `pr${branch.branchId}`
-
-        core.info(`stack name  ${stackNamePrefix}-(${xxSuffix}|${prSuffix})`)
-
-        const stackHelper = new StackHelper()
-
-        const stacks = (await stackHelper.listAllStacks(stackNamePrefix)).map((s) => s.StackName)
-
-        const xxStacks = stacks.filter((stackName) => stackName.endsWith(xxSuffix))
-        const prStacks = stacks.filter((stackName) => stackName.endsWith(prSuffix))
-
-        if (!xxStacks.length && !prStacks.length) {
-            core.notice('No Stacks to delete')
-        } else {
-            await Promise.allSettled([
-                stackHelper.deleteStacks(xxStacks, waitForDeleteComplete),
-                stackHelper.deleteStacks(prStacks, waitForDeleteComplete),
-            ])
-            core.notice(`deleted stacks: ${[...xxStacks, ...prStacks].join(', ')} (waitForDeleteComplete: ${waitForDeleteComplete})`)
-        }
-    }
+  }
 } catch (error) {
-    core.setFailed(error.message)
+  core.setFailed(error.message)
 }
--- src/stack-helper.ts
+++ src/stack-helper.ts
@@ -1,90 +1,88 @@
 import * as core from '@actions/core'
-import {setTimeout} from 'node:timers/promises'
+import { setTimeout } from 'node:timers/promises'
 import {
-    CloudFormation,
-    Stack,
-    StackStatus,
-    StackSummary,
-    waitUntilStackDeleteComplete
-} from "@aws-sdk/client-cloudformation";
+  CloudFormation,
+  Stack,
+  StackStatus,
+  StackSummary,
+  waitUntilStackDeleteComplete,
+} from '@aws-sdk/client-cloudformation'
 
 const COMPLETED_STATI: StackStatus[] = [
-    StackStatus.CREATE_COMPLETE,
-    StackStatus.UPDATE_COMPLETE,
-    StackStatus.ROLLBACK_COMPLETE,
-    StackStatus.UPDATE_ROLLBACK_COMPLETE,
-    StackStatus.IMPORT_COMPLETE,
+  StackStatus.CREATE_COMPLETE,
+  StackStatus.UPDATE_COMPLETE,
+  StackStatus.ROLLBACK_COMPLETE,
+  StackStatus.UPDATE_ROLLBACK_COMPLETE,
+  StackStatus.IMPORT_COMPLETE,
 ]
 
-const MAX_WAIT_TIME_SEC = 10 * 60 // 10 minutes 
+const MAX_WAIT_TIME_SEC = 10 * 60 // 10 minutes
 
 export class StackHelper {
-    private readonly cfn: CloudFormation
+  private readonly cfn: CloudFormation
 
-    constructor(cfn?: CloudFormation) {
-        this.cfn = cfn || new CloudFormation({})
+  constructor(cfn?: CloudFormation) {
+    this.cfn = cfn || new CloudFormation({})
+  }
+
+  async listAllStacks(stackNamePrefix: string, statusFilter: StackStatus[] = COMPLETED_STATI): Promise<StackSummary[]> {
+    const stacks: StackSummary[] = []
+    let nextToken: string | undefined = undefined
+    do {
+      const stackListResponse = await this.cfn.listStacks({
+        NextToken: nextToken,
+        StackStatusFilter: statusFilter,
+      })
+
+      const matchingStacks = stackListResponse.StackSummaries.filter((s) => s.StackName.startsWith(stackNamePrefix))
+      stacks.push(...matchingStacks)
+
+      nextToken = stackListResponse.NextToken
+    } while (nextToken)
+
+    return stacks
+  }
+
+  async deleteStacks(stackNames: string[], waitForDeleteComplete = false): Promise<void> {
+    if (stackNames.length === 0) {
+      return
     }
 
-    async listAllStacks(stackNamePrefix: string, statusFilter: StackStatus[] = COMPLETED_STATI): Promise<StackSummary[]> {
-        const stacks: StackSummary[] = []
-        let nextToken: string | undefined = undefined
-        do {
-            const stackListResponse = await this.cfn
-                .listStacks({
-                    NextToken: nextToken,
-                    StackStatusFilter: statusFilter,
-                })
+    core.info(`delete stacks: ${stackNames} (waitForDeleteComplete: ${waitForDeleteComplete})`)
+    await Promise.all(stackNames.map((s) => this.deleteStack(s, waitForDeleteComplete)))
+  }
 
-            const matchingStacks = stackListResponse.StackSummaries.filter((s) => s.StackName.startsWith(stackNamePrefix))
-            stacks.push(...matchingStacks)
-
-            nextToken = stackListResponse.NextToken
-        } while (nextToken)
-
-        return stacks
+  async deleteStack(stackName: string, waitForDeleteComplete = false) {
+    await this.cfn.deleteStack({ StackName: stackName })
+    if (waitForDeleteComplete) {
+      await waitUntilStackDeleteComplete({ client: this.cfn, maxWaitTime: MAX_WAIT_TIME_SEC }, { StackName: stackName })
     }
+  }
 
-    async deleteStacks(stackNames: string[], waitForDeleteComplete = false): Promise<void> {
-        if (stackNames.length === 0) {
-            return
+  async describeStack(stackName: string): Promise<Stack | undefined> {
+    // throws if stack does no longer exist -- with listStacks we could get the actual 'DELETE_COMPLETE' status of it
+    const stack = await this.cfn.describeStacks({ StackName: stackName })
+    return stack.Stacks.find((s) => s.StackName === stackName)
+  }
+
+  // @ts-ignore disable unused, since it is used in recursion
+  private async getStackUpdateUntilDeleted(stackName: string, first = true): Promise<boolean> {
+    const stack = await this.describeStack(stackName)
+    switch (stack?.StackStatus) {
+      case 'DELETE_COMPLETE':
+        core.info(`${stack.StackName}: ${stack.StackStatus}`)
+        return true
+      case 'DELETE_FAILED':
+        core.error(`${stack.StackName}: ${stack.StackStatus} (reason: ${stack.StackStatusReason})`)
+        return false
+      case 'DELETE_IN_PROGRESS':
+        if (first) {
+          core.info(`${stack.StackName}: ${stack.StackStatus}`)
         }
-        
-        core.info(`delete stacks: ${stackNames} (waitForDeleteComplete: ${waitForDeleteComplete})`)
-        await Promise.all(stackNames.map((s) => this.deleteStack(s, waitForDeleteComplete)))
+        await setTimeout(15)
+        return this.getStackUpdateUntilDeleted(stack.StackName, false)
+      default:
+        throw new Error(`StackStatus of ${stack.StackName} is ${stack.StackStatus} (${stack.StackStatusReason})`)
     }
-
-    async deleteStack(stackName: string, waitForDeleteComplete = false) {
-        await this.cfn.deleteStack({StackName: stackName})
-        if (waitForDeleteComplete) {
-            await waitUntilStackDeleteComplete({client: this.cfn, maxWaitTime: MAX_WAIT_TIME_SEC}, {StackName: stackName})
-        }
-    }
-
-    async describeStack(stackName: string): Promise<Stack | undefined> {
-        // throws if stack does no longer exist -- with listStacks we could get the actual 'DELETE_COMPLETE' status of it
-        const stack = await this.cfn
-            .describeStacks({StackName: stackName})
-        return stack.Stacks.find((s) => s.StackName === stackName)
-    }
-
-    // @ts-ignore disable unused, since it is used in recursion
-    private async getStackUpdateUntilDeleted(stackName: string, first = true): Promise<boolean> {
-        const stack = await this.describeStack(stackName)
-        switch (stack?.StackStatus) {
-            case 'DELETE_COMPLETE':
-                core.info(`${stack.StackName}: ${stack.StackStatus}`)
-                return true
-            case 'DELETE_FAILED':
-                core.error(`${stack.StackName}: ${stack.StackStatus} (reason: ${stack.StackStatusReason})`)
-                return false
-            case 'DELETE_IN_PROGRESS':
-                if (first) {
-                    core.info(`${stack.StackName}: ${stack.StackStatus}`)
-                }
-                await setTimeout(15)
-                return this.getStackUpdateUntilDeleted(stack.StackName, false)
-            default:
-                throw new Error(`StackStatus of ${stack.StackName} is ${stack.StackStatus} (${stack.StackStatusReason})`)
-        }
-    }
+  }
 }
